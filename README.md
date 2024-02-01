e4cf278319639917b76518da9eacfb135f8185ab
Benedict Halim <benedicthalim1@gmail.com>
2024-02-01 22:08:13 +0700

    Fixed Pythagoras

---
+++ README.md
@@ -0,0 +1,10 @@
+---- CHANGES ----
+
+> MAJOR:
+
+    - Added Pythagoras in triangle.js
+
+> MINOR:
+
+    - Formatted every -.html, -.css and -.js files to enhance readability (Using prettier)
+    - Added text-size transition to Navbar :hover animation
--- triangle/script/pythagoras.js
+++ triangle/script/pythagoras.js
@@ -5,9 +5,9 @@ function callPythagoras() {
   let a = parseFloat(document.getElementById("aPy").value);
   a = parseFloat(a.toFixed(floatAccuration));
   let b = parseFloat(document.getElementById("bPy").value);
-  b = parseFloat(a.toFixed(floatAccuration));
+  b = parseFloat(b.toFixed(floatAccuration));
   let c = parseFloat(document.getElementById("cPy").value);
-  c = parseFloat(a.toFixed(floatAccuration));
+  c = parseFloat(c.toFixed(floatAccuration));
 
   resultPythagoras.innerHTML = "";
 
@@ -22,7 +22,6 @@ function callPythagoras() {
     resultPythagoras.classList.remove("form__equation");
     let result = [];
     if (a <= 0) {
-      console.log(result);
       resultPythagoras.classList.add("red");
       result.push("Side a should be positive number");
     }
@@ -31,7 +30,6 @@ function callPythagoras() {
       result.push("Side a: Number limit exceeded");
     }
     if (b <= 0) {
-      console.log(result);
       resultPythagoras.classList.add("red");
       result.push("Side b should be positive number");
     }
@@ -40,7 +38,6 @@ function callPythagoras() {
       result.push("Side b: Number limit exceeded");
     }
     if (c <= 0) {
-      console.log(result);
       resultPythagoras.classList.add("red");
       result.push("Side c should be positive number");
     }
@@ -50,65 +47,83 @@ function callPythagoras() {
     }
     resultPythagoras.innerHTML = result.join("<br>");
   } else {
-    //True/False?
     if (!isNaN(a) && !isNaN(b) && !isNaN(c)) {
       resultPythagoras.classList.remove("red");
       let PythagorasResult = boolPythagoras(a, b, c);
-      resultPythagoras.innerHTML = PythagorasResult;
-    }
-    //Find c
-    else if (!isNaN(a) && !isNaN(b)) {
-      aborc = "c";
-      resultPythagoras.classList.remove("red");
-      PythagorasResult = calcPythagoras(a, b);
-      resultPythagoras.innerHTML = PythagorasResult;
-    }
-    //find b
-    else if (!isNaN(a) && !isNaN(c)) {
-      aborc = "b";
-      resultPythagoras.classList.remove("red");
-      PythagorasResult = calcPythagoras(a, c);
       resultPythagoras.innerHTML = PythagorasResult;
     }
     //find a
     else if (!isNaN(b) && !isNaN(c)) {
       aborc = "a";
       resultPythagoras.classList.remove("red");
-      PythagorasResult = calcPythagoras(b, c);
+      let PythagorasResult = calcPythagoras(b, c);
+      resultPythagoras.innerHTML = PythagorasResult;
+    }
+    //find b
+    else if (!isNaN(a) && !isNaN(c)) {
+      aborc = "b";
+      resultPythagoras.classList.remove("red");
+      let PythagorasResult = calcPythagoras(a, c);
+      resultPythagoras.innerHTML = PythagorasResult;
+    }
+    //find c
+    else if (!isNaN(a) && !isNaN(b)) {
+      aborc = "c";
+      resultPythagoras.classList.remove("red");
+      let PythagorasResult = calcPythagoras(a, b);
       resultPythagoras.innerHTML = PythagorasResult;
     }
   }
 }
 
-function calcPythagoras(a, b) {
+function calcPythagoras(arg1, arg2) {
   resultPythagoras.classList.add("form__equation");
-  let c = Math.sqrt(a ** 2 + b ** 2);
-  c = parseFloat(s.toFixed(floatAccuration));
+  //arg1 = b; arg2 = c;
   if (aborc == "a") {
+    let res = Math.sqrt(arg2 ** 2 - arg1 ** 2);
+    res = parseFloat(res.toFixed(floatAccuration));
     let pythagorasResult = [
-      `\\(${c}^2 = a^2 + ${b}^2)`,
-      `\\(a^2 = ${c}^2 - ${b}^2)`,
-      `\\(a = \\sqrt{${c}^2 - ${b}^2}\\)`,
-      `\\(a = ${Math.sqrt(c ** 2 - b ** 2)})`,
+      `\\(c^2 = a^2 + b^2\\)`,
+      `\\(a^2 = ${arg2}^2 - ${arg1}^2\\)`,
+      `\\(a = \\sqrt{${arg2}^2 - ${arg1}^2}\\)`,
+      `\\(a = ${res}\\)`,
     ];
+    setTimeout(function () {
+      resultPythagoras.innerHTML = pythagorasResult.join("<br>");
+      MathJax.Hub.Queue(["Typeset", MathJax.Hub, resultPythagoras]);
+    }, 0);
     return pythagorasResult;
   }
+  //arg1 = a; arg2 = c;
   if (aborc == "b") {
+    let res = Math.sqrt(arg2 ** 2 - arg1 ** 2);
+    res = parseFloat(res.toFixed(floatAccuration));
     let pythagorasResult = [
-      `\\(${c}^2 = ${a}^2 + ${b}^2)`,
-      `\\(b^2 = ${c}^2 - ${a}^2)`,
-      `\\(b = \\sqrt{${c}^2 - ${a}^2}\\)`,
-      `\\(b = ${Math.sqrt(c ** 2 - a ** 2)})`,
+      `\\(c^2 = a^2 + b^2\\)`,
+      `\\(b^2 = ${arg2}^2 - ${arg1}^2\\)`,
+      `\\(b = \\sqrt{${arg2}^2 - ${arg1}^2}\\)`,
+      `\\(b = ${res}\\)`,
     ];
+    setTimeout(function () {
+      resultPythagoras.innerHTML = pythagorasResult.join("<br>");
+      MathJax.Hub.Queue(["Typeset", MathJax.Hub, resultPythagoras]);
+    }, 0);
     return pythagorasResult;
   }
+  //arg1 = a; arg2 = b;
   if (aborc == "c") {
+    let res = Math.sqrt(arg1 ** 2 + arg2 ** 2);
+    res = parseFloat(res.toFixed(floatAccuration));
     let pythagorasResult = [
-      `\\(${c}^2 = ${a}^2 + ${b}^2)`,
-      `\\(c^2 = ${a}^2 + ${b}^2)`,
-      `\\(c = \\sqrt{${a}^2 + ${b}^2}\\)`,
-      `\\(c = ${Math.sqrt(c ** 2 + b ** 2)})`,
+      `\\(c^2 = a^2 + b^2\\)`,
+      `\\(c^2 = ${arg1}^2 + ${arg2}^2\\)`,
+      `\\(c = \\sqrt{${arg1}^2 + ${arg2}^2}\\)`,
+      `\\(c = ${res}\\)`,
     ];
+    setTimeout(function () {
+      resultPythagoras.innerHTML = pythagorasResult.join("<br>");
+      MathJax.Hub.Queue(["Typeset", MathJax.Hub, resultPythagoras]);
+    }, 0);
     return pythagorasResult;
   }
 }
